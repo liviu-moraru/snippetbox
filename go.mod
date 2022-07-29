8243a3ca84663b5374bc0f2ecf6ac7ea37e568d8
Liviu Moraru <liviu.moraru@gmail.com>
2022-07-29 13:27:06 +0300

    2.3 Routing request

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/liviu-moraru/snippetbox
+
+go 1.18
---
+++ main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+// Define a home handler function which writes a byte slice containing
+// "Hello from Snippetbox" as the response body.
+func home(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		log.Println("Request path:", r.URL.Path)
+		http.NotFound(w, r)
+		return
+	}
+	w.Write([]byte("Hello from Snippetbox"))
+}
+
+// Add a snippetView handler function.
+func snippetView(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display a specific snippet..."))
+}
+
+// Add a snippetCreate handler function.
+func snippetCreate(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Create a new snippet..."))
+}
+
+func fooView(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Foo view..."))
+}
+
+func main() {
+	// Use the http.NewServeMux() function to initialize a new servemux, then
+	// register the home function as the handler for the "/" URL pattern.
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet/view", snippetView)
+	mux.HandleFunc("/snippet/create", snippetCreate)
+	mux.HandleFunc("foo/", fooView)
+
+	log.Println("Starting server on :4000")
+	err := http.ListenAndServe(":4000", mux)
+	log.Fatal(err)
+}
