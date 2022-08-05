bef4e3c5e17780462e0516c38a6cccdd6567197b
Liviu Moraru <liviu.moraru@gmail.com>
2022-08-05 10:48:15 +0300

    3.5 Isolating the application routes

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -31,25 +31,6 @@ func main() {
 	app.ErrorLog = errorLog
 	app.InfoLog = infoLog
 
-	// Use the http.NewServeMux() function to initialize a new servemux, then
-	// register the home function as the handler for the "/" URL pattern.
-	mux := http.NewServeMux()
-
-	// Create a file server which serves files out of the "./ui/static" directory.
-	// Note that the path given to the http.Dir function is relative to the project
-	// directory root.
-	fileServer := http.FileServer(neuteredFileSystem{http.Dir(app.StaticDir)})
-
-	// Use the mux.Handle() function to register the file server as the handler for
-	// all URL paths that start with "/static/". For matching paths, we strip the
-	// "/static" prefix before the request reaches the file server.
-	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
-
-	mux.Handle("/", HomeHandler(&app))
-	mux.HandleFunc("/snippet/view", snippetView)
-	mux.HandleFunc("/snippet/create", snippetCreate)
-	mux.Handle("/handler/", &handlerImpl{})
-
 	// Initialize a new http.Server struct. We set the Addr and Handler fields so
 	// that the server uses the same network address and routes as before, and set
 	// the ErrorLog field so that the server now uses the custom errorLog logger in
@@ -57,7 +38,7 @@ func main() {
 	srv := &http.Server{
 		Addr:     app.Addr,
 		ErrorLog: app.ErrorLog,
-		Handler:  mux,
+		Handler:  routes(&app),
 	}
 
 	infoLog.Printf("Starting server on %s\n", app.Addr)
